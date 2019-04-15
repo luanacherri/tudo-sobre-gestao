bfb0a7c3110775cecedf7ffe139b057b2e78d4be
Aquinolima <49047131+Aquinolima@users.noreply.github.com>
2019-04-15 18:08:09 -0300

    falta texto, logo, formulário para leads ok

---
+++ assets/js/config.js
@@ -0,0 +1,13 @@
+/**
+ * Função para configurar firebase
+ *
+ */
+config = {
+	apiKey: "AIzaSyD37y1AZzf_WBTvEBnxuPzWUx8e8mLr14I",
+	authDomain: "blog-omie.firebaseapp.com",
+	databaseURL: "https://blog-omie.firebaseio.com",
+	projectId: "blog-omie",
+	storageBucket: "blog-omie.appspot.com",
+	messagingSenderId: "465628476142"
+};
+firebase.initializeApp(config);
---
+++ assets/js/scripts.js
@@ -0,0 +1,45 @@
+var database = firebase.database();
+
+/**
+ * Função para gravar lead no banco de dados
+ *
+ * @param {string} name, {string} email, {date} date, {string} ip, {string} type
+ */
+function saveLeads(name, email, date, ip, type) {
+
+	var ref = database.ref('leads');
+	var saveNewLead = ref.push();
+	saveNewLead.set({
+		name: name,
+		email: email,
+		currentDate: date,
+		ip: ip,
+		type: type
+	});
+}
+
+/**
+ * Função para enviar os dados para o Firebase
+ *
+ */
+form = document.getElementById('form-leads');
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	var name = form.querySelector('[name="name"]').value,
+	email = form.querySelector('[name="email"]').value,
+	ip = form.querySelector('[name="ip"]').value,
+	type = '',
+	date = new Date().toLocaleString();
+
+	var reg = /^([\w-\.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!yahoo.co.in)(?!aol.com)(?!abc.com)(?!xyz.com)(?!pqr.com)(?!rediffmail.com)(?!live.com)(?!outlook.com)(?!me.com)(?!msn.com)(?!ymail.com)([\w-]+\.)+[\w-]{2,4})?$/;
+	if (reg.test(email)){
+		type = 'b2b';
+	} else {
+		type = 'b2c'
+	}
+
+	saveLeads(name, email, date, ip, type);
+
+	form.parentNode.parentNode.classList.add('obrigado');
+
+})
